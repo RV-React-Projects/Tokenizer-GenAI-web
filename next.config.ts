555ab0d6a855e7cf0c2e704858c54e36e1fc7add
Ranvijay <ranvijaychouhan12@gmail.com>
2025-08-12 23:20:22 +0530

    revert the chnages

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,22 @@
 import type { NextConfig } from "next";
 
+const isGhPages = process.env.DEPLOY_TARGET === "gh-pages";
+const isProd = process.env.NODE_ENV === "production";
+const isVercel = process.env.VERCEL === "1";
+const repo = "Tokenizer-GenAI-web";
+
 const nextConfig: NextConfig = {
-  output: "export",
   trailingSlash: true,
+  eslint: { ignoreDuringBuilds: true },
+  reactStrictMode: true,
+  crossOrigin: "anonymous",
+  typescript: { ignoreBuildErrors: true },
   images: {
     unoptimized: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "export",
+  basePath: isVercel ? "" : isGhPages && isProd ? `/${repo}` : "",
+  assetPrefix: isVercel ? "" : isGhPages && isProd ? `/${repo}/` : "",
 };
 
 export default nextConfig;
